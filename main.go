795baf71e96884ab5ef1948539b6e245673136dc
NithanPrabhu158 <nithanprabhu158@gmail.com>
2025-06-15 10:24:46 +0530

    main.go cleanup

--- main.go
+++ main.go
@@ -5,54 +5,12 @@ import (
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
-	http.ServeFile(w, r, "static/home.html")
-}
-
-func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
-}
-
-func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
-	http.ServeFile(w, r, "static/about.html")
-}
-
-func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
-	http.ServeFile(w, r, "static/contact.html")
-}
-
-// func main() {
-
-// 	http.HandleFunc("/home", homePage)
-// 	http.HandleFunc("/courses", coursePage)
-// 	http.HandleFunc("/about", aboutPage)
-// 	http.HandleFunc("/contact", contactPage)
-
-// 	err := http.ListenAndServe("0.0.0.0:8080", nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
 
 func main() {
-    // Serve static files from 2106_soft_landing at /soft/
     fs := http.FileServer(http.Dir("landing_page"))
     http.Handle("/", http.StripPrefix("/", fs))
 
-    // Optional: Redirect root to the landing page
-    // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    //     if r.URL.Path == "/" {
-    //         http.Redirect(w, r, "/soft/index.html", http.StatusFound)
-    //         return
-    //     }
-    //     http.NotFound(w, r)
-    // })
-
-    log.Println("Server started at http://localhost:8080")
+    log.Println("Server started at :8080")
     err := http.ListenAndServe("0.0.0.0:8080", nil)
     if err != nil {
         log.Fatal(err)
